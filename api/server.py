eefb7ce97b863c2242c974b029b64bca7640a5b3
nijikaraka <jmffn317@gmail.com>
2023-12-11 22:48:33 +0900

    変更

--- api/server.py
+++ api/server.py
@@ -1,21 +1,7 @@
-# main.py
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 
-class Item(BaseModel):
-    name: str
-    price: float
-
 @app.get("/")
-async def read_root():
-    return {"message": "Hello World"}
-
-@app.get("/items/{item_id}")
-async def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
-
-@app.post("/items")
-async def create_item(item: Item):
-    return {"item_name": item.name, "twice_price": item.price * 2}
+async def root():
+    return {"message": "Hello World From Fast API"}
